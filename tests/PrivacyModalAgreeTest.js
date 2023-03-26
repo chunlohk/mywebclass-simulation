8ac153c2bc02a95cd909ec6877e6caadf30738d8
bcs25@njit.edu <bcs25@njit.edu>
2023-03-25 23:08:51 -0400

    adding tests

--- tests/PrivacyModalAgreeTest.js
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://chunlohk.github.io/mywebclass-simulation/');
-  await page.getByRole('button', { name: 'I Agree' }).click();
-});
